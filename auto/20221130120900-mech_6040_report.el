440c05ff7da938d0c3cd0952473b91d6368a6e36
dn757657 <daniel.js.campbell@gmail.com>
2022-12-04 11:38:35 -0400

    6040 report updates
    6040 report updates

--- auto/20221130120900-mech_6040_report.el
+++ auto/20221130120900-mech_6040_report.el
@@ -4,7 +4,7 @@
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("article" "11pt")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
-                     '(("inputenc" "latin1") ("fontenc" "T1") ("ulem" "normalem")))
+                     '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem") ("geometry" "a4paper" "portrait" "margin=.75in")))
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
@@ -27,36 +27,46 @@
     "amsmath"
     "amssymb"
     "capt-of"
-    "hyperref")
+    "hyperref"
+    "geometry")
    (LaTeX-add-labels
-    "sec:org7ed4d4c"
-    "sec:org4df4ac0"
-    "sec:orgb091496"
-    "sec:orgdd1802a"
-    "sec:orga8a0eb3"
-    "sec:org81558a2"
-    "sec:org02a0fe5"
-    "sec:org18a545d"
-    "sec:orgbd77c32"
-    "sec:org10aaa21"
-    "sec:org369ddd9"
-    "sec:org1b9af3e"
-    "sec:orgd5fa406"
-    "sec:org88e9ace"
-    "sec:org1e68af4"
-    "sec:org36dcff0"
-    "sec:orgcd6b2c9"
-    "sec:orge8ee5af"
-    "sec:orgc1eb78f"
-    "sec:org187371a"
-    "sec:org3b0b746"
-    "sec:org6a6e6d1"
-    "sec:orgeb58cc6"
-    "sec:orge660f46"
-    "sec:org38eecd0"
-    "sec:orge357887"
-    "sec:orga59c97c"
-    "sec:org7d65d02"
-    "sec:orgf67a789"))
+    "sec:org98177fe"
+    "sec:orgde1566f"
+    "sec:orgcfabd61"
+    "sec:org396b4de"
+    "sec:org0c80dba"
+    "sec:orgdaad11d"
+    "sec:orgeadf4bb"
+    "sec:org2cd8040"
+    "sec:org7107c7f"
+    "sec:org8b082a0"
+    "sec:org2cd26be"
+    "sec:orgd54c268"
+    "sec:org4877cca"
+    "sec:org6c111ff"
+    "sec:org5e2febe"
+    "sec:org9160ac4"
+    "sec:org8396a82"
+    "sec:org3ecc293"
+    "sec:org46886c2"
+    "sec:orgc9383c6"
+    "sec:org9d7f82e"
+    "sec:org7e8225b"
+    "sec:org5b580e1"
+    "sec:org11bcf12"
+    "sec:orgb443f4c"
+    "sec:org85135ed"
+    "sec:org1e1236f"
+    "sec:orgbaa630d"
+    "sec:org52260d1"
+    "sec:org04eae0c"
+    "sec:org62caf10"
+    "sec:orge09ad20"
+    "sec:org01972ce"
+    "sec:org4f0e220"
+    "sec:orgbdc7252"
+    "sec:org9348821"
+    "sec:orgb6f5bb7"
+    "sec:org90bc470"))
  :latex)
 
